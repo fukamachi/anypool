a7664baceb6808a9d1276a01430d5874b2ed8282
gos-k <mag4.elan@gmail.com>
2023-07-27 14:26:49 +0900

    Move to enable item-active

--- src/main.lisp
+++ src/main.lisp
@@ -139,7 +139,6 @@
                                 :limit (pool-max-open-count pool)))
                    (bt:acquire-lock lock))))
               (let ((item (dequeue storage)))
-                (setf (item-active-p item) t)
                 #+sbcl
                 (when (item-idle-timer item)
                   ;; Release the lock once to prevent from deadlock
@@ -151,6 +150,7 @@
                    (decf (pool-timeout-in-queue-count pool)))
                   ((or (null ping)
                        (funcall ping (item-object item)))
+                   (setf (item-active-p item) t)
                    (incf (pool-active-count pool))
                    (return (item-object item)))
                   ;; Not available anymore. Just ignore
